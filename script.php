80b5bdc7c49c70921b679e46f570f50f0f31e137
MaWo119 <maxi.wottke@web.de>
2022-01-09 21:29:15 +0000

    Listen für Versetzungsrechner erstellt, 5en und 6en zählen und in Variablen speichern

--- script.php
+++ script.php
@@ -6,6 +6,8 @@ echo '<form method="post">';
 $notenListe = array();
 $summe = 0;
 
+#Durchschnittsberechung
+
 for ($i = 1; $i <= 13; $i++) {
     $id = "n" . (string) $i;
     array_push($notenListe, $_POST[$id]);
@@ -15,9 +17,48 @@ for ($i = 0; $i <= 12; $i++) {
     $summe = $summe + $notenListe[$i];
 }
 
-$durchschnitt = $summe/13;
+$durchschnitt = round($summe/13, 2);
 
-print_r($summe);
-print_r(round($durchschnitt,2));
-print_r($notenListe);
+print_r($durchschnitt);
+
+#Versetzung berechnen
+
+$list = $notenListe;
+$error = 0;
+$kfaecher = $notenListe;
+$bestanden = "leer";
+
+echo "</br>";
+var_dump(array_splice($kfaecher, 0, -9));
+echo "</br>";
+var_dump(array_splice($list, 4));
+echo "</br>";
+
+$kc5 = 0;
+$kc6 = 0;
+
+$gc5 = 0;
+$gc6 = 0;
+
+#5en und 6en in Listen zählen
+
+for ($i = 0; i < sizeof($kfaecher); $i++) {
+    if ($kfaecher[$i] == 5) {
+        $kc5++;
+    }
+    if ($kfaecher[$i] == 6) {
+        $kc6;
+    }
+}
+
+for ($i = 0; i < sizeof(); $i++) {
+    if ($list[$i] == 5) {
+        $gc5++;
+    }
+    if ($list[$i] == 6) {
+        $gc6++;
+    }
+}
+
+
 ?>
