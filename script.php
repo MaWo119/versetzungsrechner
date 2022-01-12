2085c73366f5632bede9ab732f74960b77722caa
MaWo119 <maxi.wottke@web.de>
2022-01-12 20:23:02 +0000

    Ausgleichen Funktion implementiert

--- script.php
+++ script.php
@@ -19,8 +19,6 @@ for ($i = 0; $i <= 12; $i++) {
 
 $durchschnitt = round($summe/13, 2);
 
-print_r($durchschnitt);
-
 #Versetzung berechnen
 
 $list = $notenListe;
@@ -28,11 +26,8 @@ $error = 0;
 $kfaecher = $notenListe;
 $bestanden = "leer";
 
-echo "</br>";
-var_dump(array_splice($kfaecher, 0, -9));
-echo "</br>";
-var_dump(array_splice($list, 4));
-echo "</br>";
+$kfaecher = array_splice($kfaecher, 0, -9);
+$list = array_splice($list, 4);
 
 $kc5 = 0;
 $kc6 = 0;
@@ -47,7 +42,7 @@ for ($i = 0; $i < sizeof($kfaecher); $i++) {
         $kc5++;
     }
     if ($kfaecher[$i] == 6) {
-        $kc6;
+        $kc6++;
     }
 }
 
@@ -60,5 +55,72 @@ for ($i = 0; $i < sizeof($list); $i++) {
     }
 }
 
+#Check ob Schueler direkt durchgefallen ist (kein Ausgleich moeglich)
+#if (($kc6 > 0) || ($gc6 > 1) || ($kc5 > 1) || (($kc5 > 0) && ($gc5 > 1)) || ($gc5 > 2)) {
+if (false) {
+    $bestanden = "nicht versetzt";
+}
+else {
+    var_dump($list);
+    echo "</br>";
+    echo "</br>";
+    var_dump(ausgleichen($list));
+    echo "</br>";
+}
 
+function ausgleichen($l) {
+    $c1 = 0;
+    $c2 = 0;
+    $c3 = 0;
+    $c4 = 0;
+    $c5 = 0;
+    $c6 = 0;
+    for ($i = 0; $i < sizeof($l); $i++) {
+        if ($l[$i] == 1) {
+            $c1++;
+        }
+        if ($l[$i] == 2) {
+            $c2++;
+        }
+        if ($l[$i] == 3) {
+            $c3++;
+        }
+        if ($l[$i] == 5) {
+            $c5++;
+        }
+        if ($l[$i] == 6) {
+            $c6++;
+        }
+    }
+    if (($c6 > 0) && (($c2 >= 2) || ($c1 >= 1))) {
+        unset($l[array_search(6, $l)]);
+        if ($c2 >= 2) {
+            unset($l[array_search(2, $l)]);
+            unset($l[array_search(2, $l)]);
+        }
+        else {
+            unset($l[array_search(2, $l)]);
+        }
+        #rekursiver Aufruf der FKT, bis nichts mehr ausgeglichen werden kann
+        ausgleichen(array_values($l));
+        }
+    else if (($c5 > 0) && (($c3 >= 2) || ($c2 >= 1) || ($c1 >= 1))) {
+        unset($l[array_search(5, $l)]);
+        if ($c3 >= 2) {
+            unset($l[array_search(3, $l)]);
+            unset($l[array_search(3, $l)]);
+        }
+        else if ($c2 >= 1) {
+            unset($l[array_search(2, $l)]);
+        }
+        else {
+            unset($l[array_search(2, $l)]);
+        }
+        //rekursiver Aufruf der FKT, bis nichts mehr ausgeglichen werden kann
+            ausgleichen($l);
+        }
+    else {
+        return $l; 
+    }
+}
 ?>
